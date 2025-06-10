06e1af55309e9e1bb17b6e170b56da41a5788a12
julian <juli.molina@duocuc.cl>
2025-06-10 19:43:54 -0400

    Update ej5.py

--- clases fundamentos/actividad listas/ej5.py
+++ clases fundamentos/actividad listas/ej5.py
@@ -13,13 +13,13 @@ agregado en la lista.
 '''
 
 opcion = 0
+productos = []
 while opcion != 4:
     print("1. Agregar productos")
     print("2. Ver canasta")
     print("3. Ver total")
     print("4. Salir")
     opcion = int(input("Elija una opcion: "))
-    productos = []
     match opcion:
         case 1:
             print("Agregar productos")
@@ -29,24 +29,30 @@ while opcion != 4:
             print("4. Verduras")
             print("5. Pan")
             producto = int(input("Seleccione un producto: "))
+            nombre = ""
+            precio = 0
+
             match producto:
                 case 1:
                     precio = 1000
-                    print("Agua agregada al carro de compras")
+                    nombre = "Agua"
                 case 2:
                     precio = 2000
-                    print("Cerveza agregada al carro de compras")
+                    nombre = "Cerveza"
                 case 3:
                     precio = 1500
-                    print("Frutas agregadas al carro de compras")
+                    nombre = "Frutas"
                 case 4:
                     precio = 1200
-                    print("Verduras agregadas al carro de compras")
+                    nombre = "Verduras"
                 case 5:
                     precio = 800
-                    print("Pan agregado al carro de compras")
+                    nombre = "Pan"
                 case _:
                     print("Producto no valido")
+                    continue
+            productos.append({'nombre': nombre, 'precio': precio})
+            print(f"{nombre} agregado al carro de compras")
         case 2:
             print("Ver canasta")
             if productos:
